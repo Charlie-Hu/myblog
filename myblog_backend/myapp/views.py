1f6a4241ad12160de1772cbd2e6e542b6ce7c87f
Charlie-Hu <sanpang028@gmail>
2023-06-30 21:26:17 +1000

    register backend finished

--- myblog_backend/myapp/views.py
+++ myblog_backend/myapp/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
         password_again = request.POST.get('password_again')
-
+        print(username)
         if len(password) < 8 or len(username) < 8:
             return JsonResponse(
                 {'success': False, 'message': 'the length of the Passwords and Username is less than 8'})
